421df7123abe75abdf1b50f612d4b2a082e38994
MansiShitole <manasishitole14@gmail.com>
2025-08-04 15:16:37 +0530

    initialize react-vite

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-gray-900 to-gray-800">
+      <h1 className="text-4xl font-bold text-white bg-black px-6 py-3 rounded-xl shadow-lg hover:bg-gray-900 transition-all duration-300">
+        Hello World
+      </h1>
+    </div>
+  );
+}
+
+export default App;
